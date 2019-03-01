7b000a84e5a022a11bd6009908b4d2a885a1c3af
Ilya <Gorbynov0102@gmail.com>
2019-03-01 22:19:45 +0300

    first

---
+++ src/obfuscation/MainClass.java
@@ -0,0 +1,33 @@
+package obfuscation;
+ 
+public class MainClass {
+
+    public static void main(String[] args) {
+        /* Set the  look and feel */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
+    }
+
+}
---
+++ src/obfuscation/ReplaceItem.java
@@ -0,0 +1,22 @@
+package obfuscation;
+
+
+public class ReplaceItem {
+
+    private String input;
+    private String output;
+
+    public ReplaceItem(String input, String output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+    
+}
